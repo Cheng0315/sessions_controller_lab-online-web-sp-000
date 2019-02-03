8815b484caa7cbf7501b6ead82c48e6389392043
Cheng Vue <csalvue@gmail.com>
2019-02-03 09:48:45 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     if !params[:name] || params[:name].empty?
       redirect_to '/login'
     else
-      render :'application/hello'
+      session[:name] = params[:name]
     end
   end
 end
