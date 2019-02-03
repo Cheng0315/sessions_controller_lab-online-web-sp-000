68a8b04e64ed0253fae600511ca77f2d16ed4c8e
Cheng Vue <csalvue@gmail.com>
2019-02-03 09:44:52 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
   def create
     if !params[:user]
-      redirect :new
+      redirect_to :new
     else
       render :'application/hello'
     end
